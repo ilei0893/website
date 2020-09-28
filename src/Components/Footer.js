899e5be53b43d7331b2544998a8c0d647ee84f25
Ivan Lei <ilei0893@gmail.com>
2020-09-27 23:24:09 -0400

    Removed copyright (with permission of course)

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -16,19 +16,10 @@ class Footer extends Component {
 
     return (
       <footer>
-        <div className="row">
-          <div className="twelve columns">
-            <ul className="social-links">{networks}</ul>
-
-            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-            </ul>
-          </div>
-          <div id="go-top">
-            <a className="smoothscroll" title="Back to Top" href="#home">
-              <i className="icon-up-open"></i>
-            </a>
-          </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
         </div>
       </footer>
     );
