62ef3fd1a78a7bb0c14d9713f815242eeb26babc
Ivan.Lei <Ivan.Lei@cix.csi.cuny.edu>
2020-12-31 17:40:33 -0500

    Fixed snapshot describe blocks for about, contact, and footer sections

--- src/Components/Contact.test.js
+++ src/Components/Contact.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import Contact from "./Contact.js";
 import renderer from "react-test-renderer";
 
-describe("Tests the header section", () => {
+describe("Tests the contact section", () => {
   it("matches the snapshot", () => {
     const tree = renderer.create(<Contact />).toJSON();
     expect(tree).toMatchSnapshot();
